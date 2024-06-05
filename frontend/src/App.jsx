c703d2296d0bf0c96997fd99ba694951168de8da
dolly2001 <piyushsolanki18dec@gmail.com>
2024-06-05 20:22:46 +0530

    UI Design Completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,27 @@
-
+import RightPanel from "./components/common/RightPanel.jsx";
+import Sidebar from "./components/common/Sidebar.jsx";
+import LoginPage from "./pages/auth/login/LoginPage.jsx"
+import SignUpPage from "./pages/auth/signup/SignUpPage.jsx"
+import  HomePage  from "./pages/home/HomePage.jsx"
+import { Route, Routes} from 'react-router-dom';
+import NotificationPage from "./pages/notification/NotificationPage.jsx";
+import ProfilePage from "./pages/profile/ProfilePage.jsx";
 
 function App() {
 
   return (
-    <>
-    
-    </>
+    <div className="flex max-w-6xl mx-auto">
+
+      <Sidebar />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path="/notifications" element={<NotificationPage />} />
+        <Route path="/profile/:username" element={<ProfilePage />} />
+      </Routes>
+      <RightPanel />
+    </div>
   )
 }
 
